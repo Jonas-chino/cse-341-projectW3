5d939a79b5347844d5e0edcd67ca475a37f395aa
jonas chino <chinojonas99@gmail.com>
2026-05-30 18:21:22 -0400

    week 3 final

--- controllers/movies.js
+++ controllers/movies.js
@@ -3,76 +3,96 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
     //#swagger.tags=['Movies']
-    const result = await mongodb.getDatabase().db('entertainment_api').collection('movies').find();
-    result.toArray().then((movies) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(movies);
-    });
+    try {
+        const result = await mongodb.getDatabase().db('entertainment_api').collection('movies').find();
+        result.toArray().then((movies) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(movies);
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message || 'Error to obtain the movies.' });
+    }
 };
 
 const getSingle = async (req, res) => {
     //#swagger.tags=['Movies']
-    const movieId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db('entertainment_api').collection('movies').find({ _id: movieId });
-    result.toArray().then((movies) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(movies[0]);
-    });
+    try {
+        const movieId = new ObjectId(req.params.id);
+        const result = await mongodb.getDatabase().db('entertainment_api').collection('movies').find({ _id: movieId });
+        result.toArray().then((movies) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(movies[0]);
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message || 'Error to obtain the movie.' });
+    }
 };
 
 const createMovie = async (req, res) => {
     //#swagger.tags=['Movies']
-    const movie = {
-        title: req.body.title,
-        director: req.body.director,
-        releaseYear: req.body.releaseYear,
-        genre: req.body.genre,
-        rating: req.body.rating,
-        runtimeMinutes: req.body.runtimeMinutes,
-        boxOfficeMillions: req.body.boxOfficeMillions,
-        hasWonOscars: req.body.hasWonOscars
-    };
-    const response = await mongodb.getDatabase().db('entertainment_api').collection('movies').insertOne(movie);
-    
-    if (response.acknowledged) {
-        res.status(201).json(response); 
-    } else {
-        res.status(500).json(response.error || 'Ocurrió un error al crear la película.');
+    try {
+        const movie = {
+            title: req.body.title,
+            director: req.body.director,
+            releaseYear: req.body.releaseYear,
+            genre: req.body.genre,
+            rating: req.body.rating,
+            runtimeMinutes: req.body.runtimeMinutes,
+            boxOfficeMillions: req.body.boxOfficeMillions,
+            hasWonOscars: req.body.hasWonOscars
+        };
+        const response = await mongodb.getDatabase().db('entertainment_api').collection('movies').insertOne(movie);
+        
+        if (response.acknowledged) {
+            res.status(201).json(response); 
+        } else {
+            res.status(500).json(response.error || 'error to create the movie.');
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message || 'error to create the movie' });
     }
 };
 
 const updateMovie = async (req, res) => {
     //#swagger.tags=['Movies']
-    const movieId = new ObjectId(req.params.id);
-    const movie = {
-        title: req.body.title,
-        director: req.body.director,
-        releaseYear: req.body.releaseYear,
-        genre: req.body.genre,
-        rating: req.body.rating,
-        runtimeMinutes: req.body.runtimeMinutes,
-        boxOfficeMillions: req.body.boxOfficeMillions,
-        hasWonOscars: req.body.hasWonOscars
-    };
-    
-    const response = await mongodb.getDatabase().db('entertainment_api').collection('movies').replaceOne({ _id: movieId }, movie);
-    
-    if (response.modifiedCount > 0) {
-        res.status(204).send(); 
-    } else {
-        res.status(500).json(response.error || 'Ocurrió un error al actualizar la película.');
+    try {
+        const movieId = new ObjectId(req.params.id);
+        const movie = {
+            title: req.body.title,
+            director: req.body.director,
+            releaseYear: req.body.releaseYear,
+            genre: req.body.genre,
+            rating: req.body.rating,
+            runtimeMinutes: req.body.runtimeMinutes,
+            boxOfficeMillions: req.body.boxOfficeMillions,
+            hasWonOscars: req.body.hasWonOscars
+        };
+        
+        const response = await mongodb.getDatabase().db('entertainment_api').collection('movies').replaceOne({ _id: movieId }, movie);
+        
+        if (response.modifiedCount > 0) {
+            res.status(204).send(); 
+        } else {
+            res.status(500).json(response.error || 'error to update the movie');
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message || 'error to update the movie.' });
     }
 };
 
 const deleteMovie = async (req, res) => {
     //#swagger.tags=['Movies']
-    const movieId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db('entertainment_api').collection('movies').deleteOne({ _id: movieId });
-    
-    if (response.deletedCount > 0) {
-        res.status(204).send(); 
-    } else {
-        res.status(500).json(response.error || 'Ocurrió un error al eliminar la película.');
+    try {
+        const movieId = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db('entertainment_api').collection('movies').deleteOne({ _id: movieId });
+        
+        if (response.deletedCount > 0) {
+            res.status(204).send(); 
+        } else {
+            res.status(500).json(response.error || 'error to delete the movie.');
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message || 'error to delete the movie.' });
     }
 };
 
--- controllers/series.js
+++ controllers/series.js
@@ -1,78 +1,97 @@
-
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
     //#swagger.tags=['Series']
-    const result = await mongodb.getDatabase().db('entertainment_api').collection('series').find();
-    result.toArray().then((series) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(series);
-    });
+    try {
+        const result = await mongodb.getDatabase().db('entertainment_api').collection('series').find();
+        result.toArray().then((series) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(series);
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message || 'Error to obtain the serie.' });
+    }
 };
 
 const getSingle = async (req, res) => {
     //#swagger.tags=['Series']
-    const seriesId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db('entertainment_api').collection('series').find({ _id: seriesId });
-    result.toArray().then((series) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(series[0]);
-    });
+    try {
+        const seriesId = new ObjectId(req.params.id);
+        const result = await mongodb.getDatabase().db('entertainment_api').collection('series').find({ _id: seriesId });
+        result.toArray().then((series) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(series[0]);
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message || 'Error to obtain the serie.' });
+    }
 };
 
 const createSeries = async (req, res) => {
     //#swagger.tags=['Series']
-    const newSeries = {
-        title: req.body.title,
-        creator: req.body.creator,
-        releaseYear: req.body.releaseYear,
-        totalSeasons: req.body.totalSeasons,
-        totalEpisodes: req.body.totalEpisodes,
-        network: req.body.network,
-        isCurrentlyAiring: req.body.isCurrentlyAiring
-    };
-    
-    const response = await mongodb.getDatabase().db('entertainment_api').collection('series').insertOne(newSeries);
-    
-    if (response.acknowledged) {
-        res.status(201).json(response); 
-    } else {
-        res.status(500).json(response.error || 'Ocurrió un error al crear la serie.');
+    try {
+        const newSeries = {
+            title: req.body.title,
+            creator: req.body.creator,
+            releaseYear: req.body.releaseYear,
+            totalSeasons: req.body.totalSeasons,
+            totalEpisodes: req.body.totalEpisodes,
+            network: req.body.network,
+            isCurrentlyAiring: req.body.isCurrentlyAiring
+        };
+        
+        const response = await mongodb.getDatabase().db('entertainment_api').collection('series').insertOne(newSeries);
+        
+        if (response.acknowledged) {
+            res.status(201).json(response); 
+        } else {
+            res.status(500).json(response.error || 'Error to create the serie.');
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message || 'Error to create the serie.' });
     }
 };
 
 const updateSeries = async (req, res) => {
     //#swagger.tags=['Series']
-    const seriesId = new ObjectId(req.params.id);
-    const updatedSeries = {
-        title: req.body.title,
-        creator: req.body.creator,
-        releaseYear: req.body.releaseYear,
-        totalSeasons: req.body.totalSeasons,
-        totalEpisodes: req.body.totalEpisodes,
-        network: req.body.network,
-        isCurrentlyAiring: req.body.isCurrentlyAiring
-    };
-    
-    const response = await mongodb.getDatabase().db('entertainment_api').collection('series').replaceOne({ _id: seriesId }, updatedSeries);
-    
-    if (response.modifiedCount > 0) {
-        res.status(204).send(); // 
-    } else {
-        res.status(500).json(response.error || 'Ocurrió un error al actualizar la serie.');
+    try {
+        const seriesId = new ObjectId(req.params.id);
+        const updatedSeries = {
+            title: req.body.title,
+            creator: req.body.creator,
+            releaseYear: req.body.releaseYear,
+            totalSeasons: req.body.totalSeasons,
+            totalEpisodes: req.body.totalEpisodes,
+            network: req.body.network,
+            isCurrentlyAiring: req.body.isCurrentlyAiring
+        };
+        
+        const response = await mongodb.getDatabase().db('entertainment_api').collection('series').replaceOne({ _id: seriesId }, updatedSeries);
+        
+        if (response.modifiedCount > 0) {
+            res.status(204).send(); 
+        } else {
+            res.status(500).json(response.error || 'Error to update the serie.');
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message || 'Error to update the serie.' });
     }
 };
 
 const deleteSeries = async (req, res) => {
     //#swagger.tags=['Series']
-    const seriesId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db('entertainment_api').collection('series').deleteOne({ _id: seriesId });
-    
-    if (response.deletedCount > 0) {
-        res.status(204).send(); 
-    } else {
-        res.status(500).json(response.error || 'Ocurrió un error al eliminar la serie.');
+    try {
+        const seriesId = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db('entertainment_api').collection('series').deleteOne({ _id: seriesId });
+        
+        if (response.deletedCount > 0) {
+            res.status(204).send(); 
+        } else {
+            res.status(500).json(response.error || 'Error to delete the serie.');
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message || 'Error to delete the serie.' });
     }
 };
 
@@ -82,4 +101,4 @@ module.exports = {
     createSeries, 
     updateSeries, 
     deleteSeries 
-};      
+};
